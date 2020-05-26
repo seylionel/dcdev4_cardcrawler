b9985b01f8412b58737fbc6769be5f2d0f10ef5b
seylionel <seymlionel@gmail.com>
2020-05-26 15:19:37 +0200

    todo ajout des cards

---
+++ src/js/class/Board.js
@@ -0,0 +1,25 @@
+export default class {
+    // TODO propriété privée nbCards
+    constructor(nbCards) {
+      // TODO stocker dans une propriété el l'élement HTML avec l'attribut [data-board]
+      // TODO stocker nbCards || 32 dans la propriété privée nbCards
+  
+      // TODO lancement de la méthode init() de la class
+    }
+  
+    // TODO getter la propriété nbCards (3 lignes)
+  
+    // TODO création de la méthode init()
+      // TODO faire une boucle nbCards X
+        // TODO AJOUTER au html de l'élément stocké dans this.el le html suivant
+        // <div class="card">
+        //   <div class="card__holder">
+        //     <div class="card__inner">
+        //       <div class="card__item">?</div>
+        //     </div>
+        //   </div>
+        // </div>
+        // TODO on incrémenter l'index de la boucle
+      // TODO fin de la boucle
+    // TODO fin de la méthode init()
+  }
