f3fc76ad768660d1669d0cc0112d1d3985744ef1
seylionel <seymlionel@gmail.com>
2020-05-26 16:24:34 +0200

    affichage carte

--- src/js/class/Board.js
+++ src/js/class/Board.js
@@ -1,18 +1,46 @@
 export default class {
-    // TODO propriété privée nbCards
+    #nbCards;
+  
     constructor(nbCards) {
+      this.el = document.querySelector('[data-board]');
+      this.#nbCards = nbCards || 32;
+  
+      this.init();
+    }
+  
+    get nbCards() {
+      return this.#nbCards;
+    }
+  
+    init() {
+      for (let card = 0; card < this.nbCards; ++card) {
+        this.el.innerHTML += `
+          <div class="card">
+            <div class="card__holder">
+              <div class="card__inner">
+                <div class="card__item">?</div>
+              </div>
+            </div>
+          </div>
+        `;
+      }
+    }
+}
+    // TODO propriété privée nbCards
+ 
+
+
       // TODO stocker dans une propriété el l'élement HTML avec l'attribut [data-board]
       // TODO stocker nbCards || 32 dans la propriété privée nbCards
-  
       // TODO lancement de la méthode init() de la class
-    }
+
   
     // TODO getter la propriété nbCards (3 lignes)
   
     // TODO création de la méthode init()
       // TODO faire une boucle nbCards X
         // TODO AJOUTER au html de l'élément stocké dans this.el le html suivant
-        // <div class="card">
+        // <div class="card" data-card>
         //   <div class="card__holder">
         //     <div class="card__inner">
         //       <div class="card__item">?</div>
@@ -22,4 +50,3 @@ export default class {
         // TODO on incrémenter l'index de la boucle
       // TODO fin de la boucle
     // TODO fin de la méthode init()
-  }
